e2504216d75b241d0faae0ceef9fd2ad4cf685d0
Aleksander Barszczewski <alek.barszczewski@gmail.com>
2018-10-23 12:38:40 +0200

    changes within new slider component

--- stories/OldSlider.js
+++ stories/OldSlider.js
@@ -1,45 +1,45 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import { withInfo } from '@storybook/addon-info';
-import { action } from '@storybook/addon-actions';
-
-import { ThemeProvider, Card, SliderComp } from '../lib';
-
-const wrapperStyle = {
-  padding: '15px',
-};
-
-const cardStyle = {
-  width: '340px',
-  margin: '0 auto',
-};
-
-
-storiesOf('D3A/Organisms/Slider', module)
-  .add(
-    'Slider',
-    withInfo(`
-      Slider from https://github.com/react-component/slider
-
-      ~~~js
-      <SliderComp />
-      ~~~
-
-    `)(() => (
-      <ThemeProvider theme="d3a">
-        <div className="base" style={wrapperStyle}>
-          <Card style={cardStyle}>
-            <SliderComp
-              min={0}
-              max={50}
-              markerDistance={5}
-              unit="kWh"
-              onBeforeChange={action('onBeforeChange')}
-              onAfterChange={action('onAfterChange')}
-              onChange={action('onChange')}
-            />
-          </Card>
-        </div>
-      </ThemeProvider>
-    )),
-  );
+// import React from 'react';
+// import { storiesOf } from '@storybook/react';
+// import { withInfo } from '@storybook/addon-info';
+// import { action } from '@storybook/addon-actions';
+//
+// import { ThemeProvider, Card, SliderComp } from '../lib';
+//
+// const wrapperStyle = {
+//   padding: '15px',
+// };
+//
+// const cardStyle = {
+//   width: '340px',
+//   margin: '0 auto',
+// };
+//
+//
+// storiesOf('D3A/Organisms/Slider', module)
+//   .add(
+//     'Slider',
+//     withInfo(`
+//       Slider from https://github.com/react-component/slider
+//
+//       ~~~js
+//       <SliderComp />
+//       ~~~
+//
+//     `)(() => (
+//       <ThemeProvider theme="d3a">
+//         <div className="base" style={wrapperStyle}>
+//           <Card style={cardStyle}>
+//             <SliderComp
+//               min={0}
+//               max={50}
+//               markerDistance={5}
+//               unit="kWh"
+//               onBeforeChange={action('onBeforeChange')}
+//               onAfterChange={action('onAfterChange')}
+//               onChange={action('onChange')}
+//             />
+//           </Card>
+//         </div>
+//       </ThemeProvider>
+//     )),
+//   );
--- stories/Slider.js
+++ stories/Slider.js
@@ -1,45 +1,70 @@
-// import React from 'react';
-// import { storiesOf } from '@storybook/react';
-// import { withInfo } from '@storybook/addon-info';
-// import { action } from '@storybook/addon-actions';
-//
-// import { ThemeProvider, Card, SliderComp } from '../lib';
-//
-// const wrapperStyle = {
-//   padding: '15px',
-// };
-//
-// const cardStyle = {
-//   width: '340px',
-//   margin: '0 auto',
-// };
-//
-//
-// storiesOf('D3A/Organisms/Slider', module)
-//   .add(
-//     'Slider',
-//     withInfo(`
-//       Slider from https://github.com/react-component/slider
-//
-//       ~~~js
-//       <SliderComp />
-//       ~~~
-//
-//     `)(() => (
-//       <ThemeProvider theme="d3a">
-//         <div className="base" style={wrapperStyle}>
-//           <Card style={cardStyle}>
-//             <SliderComp
-//               min={0}
-//               max={24}
-//               markerDistance={1}
-//               unit="h"
-//               onBeforeChange={action('onBeforeChange')}
-//               onAfterChange={action('onAfterChange')}
-//               onChange={action('onChange')}
-//             />
-//           </Card>
-//         </div>
-//       </ThemeProvider>
-//     )),
-//   );
+import React, { Component } from 'react';
+import { storiesOf } from '@storybook/react';
+import { withInfo } from '@storybook/addon-info';
+import { action } from '@storybook/addon-actions';
+
+import { ThemeProvider, Card, SliderComp } from '../lib';
+
+const wrapperStyle = {
+  padding: '15px',
+};
+
+const cardStyle = {
+  width: '340px',
+  margin: '0 auto',
+};
+
+class Container extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      value: 0
+    }
+
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange ({ value }) {
+    this.setState({
+      value
+    })
+  }
+
+  render () {
+    const { value } = this.state
+
+    return (
+      <SliderComp
+        minVal={0}
+        maxVal={24}
+        unit="h"
+        name="Slider"
+        value
+        markerStep={1}
+        labelStep={12}
+        onChange={this.handleChange}
+      />)
+  }
+}
+
+storiesOf('D3A/Organisms/Slider', module)
+  .add(
+    'Slider',
+    withInfo(`
+      Slider from https://github.com/react-component/slider
+
+      ~~~js
+      <SliderComp />
+      ~~~
+
+    `)(() => (
+      <ThemeProvider theme="d3a">
+        <div className="base" style={wrapperStyle}>
+          <Card style={cardStyle}>
+            <Container/>
+          </Card>
+        </div>
+      </ThemeProvider>
+    )),
+  );
